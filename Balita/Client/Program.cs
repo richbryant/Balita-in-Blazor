7030cf9c75322d5a91b71fd0083183e686df3dd6
Rich Bryant <rich.bryant@live.co.uk>
2020-06-01 20:38:37 +0100

    Add project files.

---
+++ Balita/Client/Program.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Balita.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services
+                .AddBlazorise( options =>
+                {
+                    options.ChangeTextOnKeyPress = true;
+                } )
+                .AddBootstrapProviders()
+                .AddFontAwesomeIcons();
+
+            builder.Services.AddHttpClient("Balita.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+            // Supply HttpClient instances that include access tokens when making requests to the server project
+            builder.Services.AddTransient(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Balita.ServerAPI"));
+
+            builder.Services.AddApiAuthorization();
+
+            var host = builder.Build();
+
+            host.Services
+                .UseBootstrapProviders()
+                .UseFontAwesomeIcons();
+
+            await host.RunAsync();
+        }
+    }
+}
---
+++ Balita/Client/Shared/CardOverlay.razor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace Balita.Client.Shared
+{
+    public partial class CardOverlay
+    {
+        [Parameter]
+        public string LinkUrl { get; set; } = "/";
+
+        [Parameter]
+        public string Image { get; set; } = "images/img_1.jpg";
+
+        [Parameter]
+        public string Category { get; set; } = "Lifestyle";
+
+        [Parameter]
+        public string PostDate { get; set; } = "June 1st 2020";
+
+        [Parameter]
+        public int CommentCount { get; set; } = 0;
+
+        [Parameter]
+        public string HeadingText { get; set; } = string.Empty;
+
+        public string BackgroundStyle { get; private set; }
+
+        protected override Task OnParametersSetAsync()
+        {
+            if (!string.IsNullOrWhiteSpace(Image)) BackgroundStyle = $"background-image:url('{Image}');";
+            return base.OnParametersSetAsync();
+        }
+    }
+}
