8bf3da1b7912690707a41ba4309f02d9a8adca2b
Rich Bryant <rich.bryant@live.co.uk>
2020-06-02 14:37:06 +0100

    Fromt page intact

---
+++ Balita/Client/Shared/HorizontalCard.razor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace Balita.Client.Shared
+{
+    public partial class HorizontalCard
+    {
+        [Parameter] 
+        public string LinkUrl { get; set; } = "/";
+
+        [Parameter] 
+        public string ImageUrl { get; set; }
+
+        [Parameter] 
+        public string Category { get; set; } = "Lifestyle";
+
+        [Parameter] 
+        public string PostedDate { get; set; } = DateTime.Today.ToString("d MMMM yyyy");
+
+        [Parameter] 
+        public int CommentCount { get; set; } = 0;
+        
+        [Parameter] 
+        public string PostHeadline { get; set; } = string.Empty;
+    }
+}
---
+++ Balita/Client/Shared/SidebarPostEntry.razor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Balita.Client.Shared
+{
+    public partial class SidebarPostEntry
+    {
+        [Parameter] 
+        public string LinkUrl { get; set; } = "/";
+
+        [Parameter] 
+        public string ImageUrl { get; set; }
+
+        [Parameter] 
+        public string Category { get; set; } = "Lifestyle";
+
+        [Parameter] 
+        public string PostedDate { get; set; } = DateTime.Today.ToString("d MMMM yyyy");
+
+        [Parameter] 
+        public int CommentCount { get; set; } = 0;
+        
+        [Parameter] 
+        public string PostHeadline { get; set; } = string.Empty;
+    }
+}
